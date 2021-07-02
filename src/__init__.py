0500bddac99231565d331078807e830a4e342976
KatherineCaley <u6096186@anu.edu.au>
2021-07-02 10:05:46 +1000

    MAINT: moved __init__.py into kath_analysis directory, fixes #7

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__ = "Katherine Caley"
-__credits__ = ["Katherine Caley"]
-__version__ = "2021.07.01"
-__maintainer__ = "Katherine Caley"
-__email__ = "katherine.caley@anu.edu.au"
-__status__ = "develop"
-
-
-version = __version__
