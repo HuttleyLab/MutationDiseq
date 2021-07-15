5fd2a73a8be1ccc0e4cbb19851573ebf3b079d3d
KatherineCaley <u6096186@anu.edu.au>
2021-07-15 19:10:27 +1000

    TST

--- tests/test_eop.py
+++ tests/test_eop.py
@@ -3,7 +3,7 @@ import pytest
 from cogent3 import make_aligned_seqs
 from cogent3.app import io
 
-from kath_library.eop import EOP
+from kath_library.eop import adjacent_EOP, edge_EOP
 
 
 @pytest.fixture()
@@ -62,23 +62,34 @@ def diff_length_alns():
 loader = io.load_db()
 
 
-def test_construction(dstore_instance):
+def test_adjacent_EOP_construction(dstore_instance):
 
     aln1 = loader(dstore_instance[0])
     aln2 = loader(dstore_instance[1])
 
-    eop = EOP([aln1, aln2])
+    eop = adjacent_EOP([aln1, aln2])
 
     assert isinstance(eop.LR, float)
     assert 0 <= eop.get_LRT_stats().to_dict(flatten=True)[(0, "p")] <= 1
 
 
-def test_eop_same_aln(dstore_instance):
+def test_edge_EOP_construction(dstore_instance):
+
+    aln = loader(dstore_instance[0])
+    names = ["758", "443154"]
+
+    eop = edge_EOP(aln, names)
+
+    assert isinstance(eop.LR, float)
+    assert 0 <= eop.get_LRT_stats().to_dict(flatten=True)[(0, "p")] <= 1
+
+
+def test_adjacent_EOP_same_aln(dstore_instance):
 
     aln1 = loader(dstore_instance[1])
     aln2 = loader(dstore_instance[1])
 
-    lr = EOP([aln1, aln2]).LR
+    lr = adjacent_EOP([aln1, aln2]).LR
     numpy.testing.assert_almost_equal(lr, 0, decimal=5)
 
 
@@ -89,27 +100,27 @@ def test_get_rel_entropies_same_aln(dstore_instance):
     aln1 = loader(dstore_instance[1])
     aln2 = loader(dstore_instance[1])
 
-    eop = EOP([aln1, aln2])
+    eop = adjacent_EOP([aln1, aln2])
     re = list(eop.get_relative_entropies().values())
 
     numpy.testing.assert_almost_equal(re[0], re[1], decimal=4)
 
 
-def test_eop_three_alns(multiple_alns):
-    eop = EOP(multiple_alns)
+def test_adjacent_EOP_three_alns(multiple_alns):
+    eop = adjacent_EOP(multiple_alns)
     assert isinstance(eop.LR, float)
     assert 0 <= eop.get_LRT_stats().to_dict(flatten=True)[(0, "p")] <= 1
     eop.get_relative_entropies()
 
 
-def test_eop_different_lengths(diff_length_alns):
-    eop = EOP(diff_length_alns)
+def test_adjacent_EOP_different_lengths(diff_length_alns):
+    eop = adjacent_EOP(diff_length_alns)
     assert isinstance(eop.LR, float)
     assert 0 <= eop.get_LRT_stats().to_dict(flatten=True)[(0, "p")] <= 1
     eop.get_relative_entropies()
 
 
-def test_eop_short_seq():
+def test_adjacent_EOP_short_seq():
     dstore = io.get_data_store(
         "~/repos/data/microbial/synthetic/758_443154_73021/300bp.tinydb"
     )
@@ -117,4 +128,4 @@ def test_eop_short_seq():
     aln1 = loader(dstore[9])
     aln2 = loader(dstore[10])
 
-    eop = EOP([aln1, aln2])
+    adjacent_EOP([aln1, aln2])
