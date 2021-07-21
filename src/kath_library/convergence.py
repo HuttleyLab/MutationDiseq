684a79c8779a3ceb660b615a3e1ad8e2938f238c
KatherineCaley <u6096186@anu.edu.au>
2021-07-21 17:35:08 +1000

    NEW: convergence calculated as rate of change, #13

--- src/kath_library/convergence.py
+++ src/kath_library/convergence.py
@@ -1,10 +1,17 @@
 from numpy.linalg import eig
+from scipy.linalg import expm
+from numpy.linalg import norm
+from numpy import array
+from accupy import fdot as dot
+from cogent3.app.composable import SERIALISABLE_TYPE, user_function
+from cogent3.app.result import generic_result
 
 __author__ = "Katherine Caley"
 __credits__ = ["Katherine Caley"]
 __version__ = "2021.07.17"
 
-def convergence(Q):
+
+def eigII(Q):
     """
     Returns the absolute value of the second largest eigenvalue from the rate matrix
     """
@@ -12,6 +19,43 @@ def convergence(Q):
     v = v.flatten()
     v.sort()
     eigII = v[-2]
-    conv = abs(eigII)
+    if isinstance(eigII, complex):
+        eigII = eigII.real
+
+    return eigII
+
+
+def convergence(pi_0, Q, t):
+    """
+    a measure of how fast pi(t) is changing
+    """
+    pi_deriv = dot(pi_0, dot(Q, expm(Q*t)))
+    conv = norm(pi_deriv)
 
     return conv
+
+
+def _get_convergence(mc):
+
+    gn = mc["mcr"]["GN"]
+    fg_edge = mc["mcr"].source["fg_edge"]
+
+    Q_darray = gn.lf.get_rate_matrix_for_edge(fg_edge, calibrated=False)
+    pi_darray = gn.alignment.counts_per_seq().to_freq_array()[fg_edge]
+
+    pi = array([pi_darray[i] for i in Q_darray.keys()])
+    Q = Q_darray.to_array()
+
+    t = gn.lf.get_param_value("length", edge=fg_edge)
+
+    conv = convergence(pi, Q, t)
+
+    result = generic_result(source=mc.source)
+    result.update([("convergence", conv), ("fg_edge", fg_edge), ("source", mc.source)])
+
+    return result
+
+
+get_convergence = user_function(
+    _get_convergence, input_types=SERIALISABLE_TYPE, output_types=SERIALISABLE_TYPE
+)
