5382286bdf2e70f63a8f2d637d383993c3d76257
KatherineCaley <u6096186@anu.edu.au>
2021-07-29 16:31:42 +1000

    VER: versioned 2021.07.29

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 __author__ = "Katherine Caley"
 __credits__ = ["Katherine Caley"]
-__version__ = "2021.07.23"
+__version__ = "2021.07.29"
 if sys.version_info < (3, 6):
     py_version = ".".join([str(n) for n in sys.version_info])
     raise RuntimeError(
