835d0e6b1d12c82cf5aca8d79f8d12e31e711809
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2021-12-21 17:28:59 +1100

    BUG: never use a mutable default value

--- src/mdeq/model.py
+++ src/mdeq/model.py
@@ -7,32 +7,26 @@ def GS_instance():
     """
     A General Stationary Nucleotide substitution model instance.
     """
-    GS = GeneralStationary(
+    return GeneralStationary(
         get_moltype("dna").alphabet, optimise_motif_probs=True, name="GS"
     )
 
-    return GS
 
+def GS_sm(discrete_edges=None):
 
-def GS_sm(discrete_edges=[]):
-
-    GS = evo.model(
+    return evo.model(
         GS_instance(),
         sm_args=dict(optimise_motif_probs=True),
         opt_args=dict(max_restarts=5, tolerance=1e-8),
         lf_args=dict(discrete_edges=discrete_edges, expm="pade"),
     )
 
-    return GS
 
+def GN_sm(discrete_edges=None):
 
-def GN_sm(discrete_edges=[]):
-
-    GN = evo.model(
+    return evo.model(
         "GN",
         sm_args=dict(optimise_motif_probs=True),
         opt_args=dict(max_restarts=5, tolerance=1e-8),
         lf_args=dict(discrete_edges=discrete_edges, expm="pade"),
     )
-
-    return GN
