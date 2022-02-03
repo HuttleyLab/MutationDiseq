f3375dd006cbebee3b73438cb2a7e56ad25a1b34
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-02-03 13:43:39 +1100

    ENH: block automated numpy threading

--- src/mdeq/__init__.py
+++ src/mdeq/__init__.py
@@ -1,2 +1,17 @@
 """mdeq: mutation disequilibrium analysis tools"""
+
+# following line to stop automatic threading by numpy
+from . import _block_threading  # isort: skip  # make sure this stays at the top
+from warnings import filterwarnings
+
+from . import (
+    model as _model,  # to ensure registration of define substitution models
+)
+
+
 __version__ = "2021.12.20"
+
+filterwarnings("ignore", "Not using MPI")
+filterwarnings("ignore", "Unexpected warning from scipy")
+filterwarnings("ignore", "using slow exponentiator")
+filterwarnings("ignore", ".*decreased to keep within bounds")
