a9a51601747b0a2285a3a6d82965f42a904bddeb
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-03-07 07:57:32 +1100

    ENH: make_identifier() function for naming adjacent data sets

--- src/mdeq/adjacent.py
+++ src/mdeq/adjacent.py
@@ -19,3 +19,26 @@ def sequential_groups(data, num: int) -> tuple[tuple[T, ...], ...]:
         raise ValueError(f"len of data {len(data)} < {num=}")
     return tuple(tuple(data[i : i + num]) for i in range(len(data) - num + 1))
 
+
+def make_identifier(data) -> str:
+    """identifies source for each element in data and makes ordered identifier,
+    no suffix
+    Returns
+    -------
+    e1--e2... (double hyphen between names without suffix)
+    """
+    from cogent3.app.data_store import get_data_source
+
+    sources = [get_data_source(e) for e in data]
+    if "unknown" in sources:
+        raise ValueError(f"'unknown' source present in {sources}")
+
+    composite = []
+    for e in sources:
+        e = Path(e)
+        l = -len(e.suffix) or None
+        composite.append(e.name[:l])
+
+    return "--".join(composite)
+
+
--- tests/test_adjacent.py
+++ tests/test_adjacent.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pytest
 
 from mdeq.adjacent import (
+    make_identifier,
     sequential_groups,
 )
 
@@ -35,5 +36,26 @@ def test_sequential_groups():
     assert got == ((0, 1, 2), (1, 2, 3))
     got = sequential_groups(data, 4)
     assert got == ((0, 1, 2, 3),)
+
+
+def test_make_identifier():
+    a = make_unaligned_seqs({"a": "ACGG"}, info=dict(source="blah/a.json"))
+    b = make_unaligned_seqs({"a": "ACGG"}, info=dict(source="blah/b.json"))
+    c = make_unaligned_seqs({"a": "ACGG"}, info=dict(source="blah/c.json"))
+    d = make_unaligned_seqs({"a": "ACGG"})
+
+    # missing identifier
+    with pytest.raises(ValueError):
+        make_identifier([a, b, d])
+
+    with pytest.raises(ValueError):
+        make_identifier([d, a])
+
+    got = make_identifier([a])
+    assert got == "a"
+    got = make_identifier([a, c])
+    assert got == "a--c"
+    got = make_identifier([a, b, c])
+    assert got == "a--b--c"
 
 
