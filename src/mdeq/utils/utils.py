5b949442b6f54ec80e4817160b675e51ea94bc5b
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-03-08 08:09:39 +1100

    API: split utils.get_foreground into 2 functions
    [CHANGED] get_foreground() now just returns processes the info
        attribute of alignments
    [NEW] foreground_from_jsd() logic identifies an ingroup lineage
        as the foreground based on Jensen-Shannon divergence

--- src/mdeq/utils/utils.py
+++ src/mdeq/utils/utils.py
@@ -16,22 +16,30 @@ def get_pi_tip(model_result, fg_edge):
 
 
 def get_foreground(aln):
+    """returns fg_edge value from info attribute"""
     try:
-        fg_edge = aln.info.fg_edge
+        fg = aln.info.get("fg_edge", None)
     except AttributeError:
-        fg_edge = None
+        fg = None
+    return fg
 
-    if fg_edge is not None:
-        return fg_edge
+
+def foreground_from_jsd(aln):
+    """returns the ingroup lineage with maximal JSD
+
+    Notes
+    -----
+    Identifies the ingroup based on conventional genetic distance,
+    identifies ingroup which has maximal JSD from the rest.
+    """
+    if aln.num_seqs != 3:
+        raise NotImplementedError()
 
     freqs = aln.counts_per_seq().to_freq_array()
     jsd_pwise = freqs.pairwise_jsd()
     darr = DictArray(jsd_pwise)
     jsd_totals = darr.row_sum().to_dict()
-
     tip_dists = aln.distance_matrix().to_dict()
     ingroup = min(tip_dists, key=lambda k: tip_dists[k])
     jsd_totals = {key: jsd_totals[key] for key in ingroup}
-    fg_edge = max(jsd_totals, key=lambda k: jsd_totals[k])
-
-    return fg_edge
+    return max(jsd_totals, key=lambda k: jsd_totals[k])
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,22 +2,32 @@ import pytest
 
 from cogent3 import make_aligned_seqs
 
-from mdeq.utils.utils import get_foreground
+from mdeq.utils.utils import foreground_from_jsd, get_foreground
 
 
 @pytest.fixture()
 def aln():
-
     _seqs = {
         "Human": "GCCAGCTCATTACAGCATGAGAACAGCAGTTTATTACTCACT",
         "Bandicoot": "GACTCATTAATGCTTGAAACCAGCAGTTTATTGTCCAACACT",
         "Rhesus": "GCCAGCTCATTACAGCATGAGAACAGTTTGTTACTCACTATT",
+        "Gorilla": "GCCAGCTCATTACAGCATGAGAACAGTTTGTTACTCACTATT",
     }
-
     return make_aligned_seqs(_seqs, moltype="dna")
 
 
-def test_doesnt_fail(aln):
-
+def test_get_foreground(aln):
+    "returns None if no key in info"
     fg_edge = get_foreground(aln)
-    print(fg_edge)
+    assert fg_edge is None
+
+
+def test_foreground_from_jsd(aln):
+    "with no foreground, identifies ingrouped lineage"
+    # but fails if > 3 sequences
+    with pytest.raises(NotImplementedError):
+        foreground_from_jsd(aln)
+
+    aln = aln.take_seqs(["Human", "Bandicoot", "Rhesus"])
+    fg_edge = foreground_from_jsd(aln)
+    assert fg_edge is not None
