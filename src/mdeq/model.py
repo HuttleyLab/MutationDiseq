e4550ac16e7ddfe2fae92c9e844e6e870319362a
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-03-16 12:40:37 +1100

    ENH: added model.mles_near_bounds for checking pamatere estimates within bounds

--- src/mdeq/model.py
+++ src/mdeq/model.py
@@ -1,7 +1,9 @@
 from cogent3 import get_moltype
 from cogent3.app import evo
+from cogent3.app.composable import SERIALISABLE_TYPE, NotCompleted, appify
 from cogent3.evolve.models import register_model
 from cogent3.evolve.ns_substitution_model import GeneralStationary
+from numpy import allclose
 
 
 __author__ = "Katherine Caley"
@@ -43,3 +45,27 @@ def GN_sm(tree=None, discrete_edges=None, opt_args=None):
         lf_args=dict(discrete_edges=discrete_edges, expm="pade"),
         upper=RATE_PARAM_UPPER,
     )
+
+
+@appify(SERIALISABLE_TYPE, SERIALISABLE_TYPE)
+def mles_near_bounds(result, lower=1e-5, upper=RATE_PARAM_UPPER):
+    """
+    Returns a NotCompleted if parameter estimates of model_result are at the bounds, else returns model_result.
+
+    """
+    tables = result.lf.get_statistics()
+    for table in tables:
+        # if time-het model, rate params in table with "edge params" title
+        # otherwise, rate params in table with "global params" title
+        if table.title in ("edge params", "global params"):
+            arr = table[:, [c for c in table.columns if c != "length"]].array
+            if not all([(arr.min() - lower) > 0.0, (upper - arr.max()) > 0.0]):
+                minval = arr.min()
+                maxval = arr.max()
+                return NotCompleted(
+                    "FAIL",
+                    "params_near_bounds",
+                    f"({minval:.1e}, {{maxval:.1f}}) params not within bounds ({lower:.1e}, {upper:.1f})",
+                    source=str(result.source),
+                )
+    return result
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from cogent3 import get_model
 
 from mdeq import model
@@ -5,6 +7,8 @@ from mdeq import model
 
 __author__ = "Katherine Caley"
 __credits__ = ["Katherine Caley", "Gavin Huttley"]
+
+DATADIR = pathlib.Path(__file__).parent / "data"
 
 
 def test_get_gsn():
@@ -25,3 +29,40 @@ def test_make_gsn_app():
 
     app = model.GS_sm()
     assert isinstance(app._sm, GeneralStationary)
+
+
+def test_mles_at_bounds():
+    from cogent3.app import composable, io
+
+    loader = io.load_db()
+    path = DATADIR / "toe-300bp.tinydb"
+    dstore = io.get_data_store(path, limit=1)
+    models = [loader(dstore[0]).observed[mod] for mod in ("GSN", "GN")]
+    app = model.mles_near_bounds()
+    for mod in models:
+        got = app(mod)
+        assert got is mod
+
+    # set lower value of 1.0 (these models have min val of 1), should return NotCompleted
+    app = model.mles_near_bounds(lower=1.0)
+    for mod in models:
+        got = app(mod)
+        assert isinstance(got, composable.NotCompleted)
+
+    # set upper value of 10 (both models have upper vals >20), should return NotCompleted
+    app = model.mles_near_bounds(upper=10)
+    for mod in models:
+        got = app(mod)
+        assert isinstance(got, composable.NotCompleted)
+
+    # set both lower and upper, should return NotCompleted
+    app = model.mles_near_bounds(lower=1, upper=10)
+    for mod in models:
+        got = app(mod)
+        assert isinstance(got, composable.NotCompleted)
+
+    # value of length should trigger NotCompleted
+    mod.lf.set_param_rule("length", init=1e-6)
+    app = model.mles_near_bounds(lower=0.5)
+    got = app(mod)
+    assert got is mod
