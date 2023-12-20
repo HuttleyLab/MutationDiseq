46d25ea07d6f52c7b9aa6defc2a896cb83e4b680
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-12-20 11:03:38 +1100

    DEV: deleted redundant line in noxfile

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,6 @@ _py_versions = range(10, 12)
 @nox.session(python=[f"3.{v}" for v in _py_versions])
 def test(session):
     session.install(".[test]")
-    session.install(".")
     session.chdir("tests")
     session.run(
         "pytest",
