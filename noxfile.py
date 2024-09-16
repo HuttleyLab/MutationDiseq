eee24603405f5e3d4d0794e1319c827bf6890106
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-09-17 09:43:41 +1000

    TST: added nox function for testing against cogent3 develop branch

--- noxfile.py
+++ noxfile.py
@@ -12,6 +12,17 @@ def test(session):
         "-x",
     )
 
+@nox.session(python=["3.12"])
+def test_dev(session):
+    """runs against cogent3 develop branch"""
+    session.install(".[test]")
+    session.install("cogent3 @ git+https://github.com/cogent3/cogent3.git@develop", "--no-cache-dir")
+    session.chdir("tests")
+    session.run(
+        "pytest",
+        "-x",
+        *session.posargs,  # propagates sys.argv to pytest
+    )
 
 @nox.session(python=[f"3.{v}" for v in _py_versions])
 def htmlcov(session):
